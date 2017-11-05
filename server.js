786b43b287b13da61857571a4cc9d87cd4b9ab78
d-mclean <darc.mclean@gmail.com>
2017-11-05 07:37:01 +0000

    Updated code after initial cleanup

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// 20171105 DM - server component for chatty app (Lighthouse Labs project).
 // server.js
 
 const express = require('express');
@@ -10,46 +11,78 @@ const PORT = 3001;
 
 // Create a new express server
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
-  .use(express.static('public'))
-  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+    // Make the express server serve static assets (html, javascript, css) from the /public folder
+    .use(express.static('public'))
+    .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
 // Create the WebSockets server
 const wss = new SocketServer({ server });
+
+// Set global variable to keep track of the users online.
+let arrUserList = [];
 
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
-  console.log('Client connected');
+    // Print out the current total to the server then boardcast it to each user.
+    console.log(`Client connected (${wss.clients.size} users)`);
+    arrUserList.push('Anonymous');
+    updateUserList();
 
-//   ws.onmessage = function(event) {
-//         console.log(event.data);
-//         console.log(JSON.parse(event.data));
-//       };
-var WebSocket = require('ws')
-      ws.on('message', function incoming(data) {
+    var WebSocket = require('ws')
+    ws.on('message', function incoming(data) {
+        let objMessage = JSON.parse(data);
+        switch(objMessage.type){
+            case 'message':
+            case 'notification':
+                // Remove the old username from the userlist.
+                let oldIndex = arrUserList.indexOf(objMessage['username']);
+                if (oldIndex > -1){
+                    arrUserList.splice(oldIndex, 1);
+                }
+                arrUserList.push(objMessage['newName']);
+                objMessage['userList'] = arrUserList;
+                break;
+            case 'update':
+                break;
+            default:
+
+            throw new Error('unknown message type ' + objMessage.type);
+
+        }
+
         // Broadcast to everyone else.
         wss.clients.forEach(function each(client) {
-          //if (client !== ws && client.readyState === WebSocket.OPEN) {
-            client.send(data);
-            console.log(data);
-          //}
+            client.send(JSON.stringify(objMessage));
         });
-      });
+    });
 
-// Broadcast to all.
-ws.broadcast = function broadcast(data) {
-  ws.clients.forEach(function each(client) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(data);
-    }
-  });
-};
+    // Broadcast to all.
+    ws.broadcast = function broadcast(data) {
+        ws.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+            client.send(data);
+            }
+        });
+    };
 
-
-
-  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+    // Set up a callback for when a client closes the socket. This usually means they closed their browser.
+    ws.on('close', () => {
+        console.log(`Client disconnected (${wss.clients.size} users)`);
+        updateUserList();
+    });
 });
 
+// Create a new message for the user list then broadcast the new total to all users.  
+function updateUserList(){
+    let oldIndex = arrUserList.indexOf('Anonymous');
+    if (oldIndex === -1){
+        arrUserList.push('Anonymous'); 
+    }
+
+    const newMessage = JSON.stringify({type: 'update', uuid: uuidv4(), username: 'Server Notification', totalUsers: wss.clients.size, userList: arrUserList});
+    wss.clients.forEach(function each(client) {
+        client.send(newMessage);
+    }) 
+}
